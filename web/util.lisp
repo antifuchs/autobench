f117330e21f754b7343a2e9611b0449d40c929de
Andreas Fuchs <asf@boinkor.net>
2005-05-27 23:28:54 +0000

    argh. fix TRANSLATE* to return the SQL
    argh. fix TRANSLATE* to return the SQL
    
    git-archimport-id: asf@boinkor.net--2004h/sb-bench--main--0--patch-52

--- web/util.lisp
+++ web/util.lisp
@@ -5,7 +5,7 @@
 (defun translate* (sexql)
   (let ((sql (translate sexql)))
     (if *debugging*
-        (print sexql)
-        sexql)))
+        (print sql)
+        sql)))
 
 ;;; arch-tag: "49c8b13b-2c1f-4b90-b6c9-24f5c381e303"
