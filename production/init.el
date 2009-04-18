3af37ee26ac94338cfff96ab0b9166009424ca17
Andreas Fuchs <asf@boinkor.net>
2009-04-18 19:02:49 +0200

    Add emacs init file

---
+++ production/init.el
@@ -0,0 +1,29 @@
+(require 'cl)
+
+;;; elisp setup
+
+(defun xach-generic-code-setup ()
+  (setq indent-tabs-mode nil)
+  (local-set-key "\C-m" 'newline-and-indent))
+
+(add-hook 'emacs-lisp-mode-hook
+	  (defun xach-emacs-setup ()
+	    (interactive)
+	    (xach-generic-code-setup)
+	    (eldoc-mode t)))
+
+(setq indent-tabs-mode nil)
+
+;;; CL setup
+
+(push "/opt/lisp/clbuild/source/slime" load-path)
+(require 'slime)
+(slime-setup '(slime-autodoc slime-fancy-inspector slime-editing-commands))
+(setq slime-port 7717)
+
+(add-hook 'lisp-mode-hook
+          (defun xach-lisp-setup ()
+            (interactive)
+            (xach-generic-code-setup)))
+
+(global-set-key "\C-cs" 'slime-selector)
