00031824cc9348f11b447bcc9cb3225720208b21
Andreas Fuchs <asf@boinkor.net>
2009-04-19 20:58:06 +0200

    Allow building of implementations in the server instance (works well enough)

--- production/autobench-init.lisp
+++ production/autobench-init.lisp
@@ -1,2 +1,7 @@
 (setf *db-default-user-name* "autobench")
 (setf *base-dir* #p"/opt/lisp/autobench/")
+
+(defstrategy +sbcl-64-release+ sbcl (:mode (:arch :x86_64 :features ()))
+             :build-p #'release-p)
+(defparameter *releases-to-build* `((sbcl :directory #p"/space/lisp/autobench/+sbcl-co/sbcl/"
+                                     :strategies (,+sbcl-64-release+))))
