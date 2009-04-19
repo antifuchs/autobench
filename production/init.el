4a59f1444f8ddf9b3a579b2bb157b33953c24abe
Andreas Fuchs <asf@boinkor.net>
2009-04-20 01:44:09 +0200

    More useful editing commands (paredit, no backup files)

--- production/init.el
+++ production/init.el
@@ -14,12 +14,35 @@
 
 (setq indent-tabs-mode nil)
 
+(setq make-backup-files nil)
+
 ;;; CL setup
 
 (push "/opt/lisp/clbuild/source/slime" load-path)
+(push "/home/autobench/.elisp" load-path)
+(require 'paredit)
 (require 'slime)
 (slime-setup '(slime-fancy slime-autodoc slime-fancy-inspector
                slime-editing-commands))
+
+(defun turn-on-paredit-mode ()
+    (paredit-mode +1))
+
+(add-hook 'lisp-mode-hook 'turn-on-paredit-mode)
+(add-hook 'slime-repl-mode-hook 'turn-on-paredit-mode)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-paredit-mode)
+
+;;; RET on the REPL: Only send lines when at end of expression,
+;;; newline and indent otherwise.
+(defun asf-slime-repl-return (&optional end-of-input)
+    (interactive "P")
+      (if (= (point) (point-max))
+                (slime-repl-return end-of-input)
+              (paredit-newline)))
+
+(define-key slime-repl-mode-map [(return)] 'asf-slime-repl-return)
+
+
 (setq slime-port 7717)
 
 (add-hook 'lisp-mode-hook
