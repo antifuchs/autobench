996dd62ab26fa8bd9a761e5f893ac3ac77d618f5
Andreas Fuchs <asf@boinkor.net>
2009-05-07 11:30:45 +0200

    add rebuildable sbcl-32 impls

--- production/autobench-init.lisp
+++ production/autobench-init.lisp
@@ -1,7 +1,10 @@
 (setf *db-default-user-name* "autobench")
 (setf *base-dir* #p"/opt/lisp/autobench/")
 
-(defstrategy +sbcl-64-release+ sbcl (:mode (:arch :x86_64 :features ()))
+
+(defstrategy +sbcl-32/rebuild+ sbcl (:mode (:arch :emulated-x86 :features ())) :build-p #'release-p)
+(defstrategy +sbcl-32-threaded/rebuild+ sbcl (:mode (:arch :emulated-x86 :features (:sb-thread)))
              :build-p #'release-p)
-(defparameter *releases-to-build* `((sbcl :directory #p"/space/lisp/autobench/+sbcl-co/sbcl/"
-                                     :strategies (,+sbcl-64-release+))))
+
+(defparameter *32impls-to-rebuild* `((sbcl :directory #p "/home/sbcl-arch/space/autobench/+newest-64/"
+                                      :strategies (,+sbcl-32/rebuild+ ,+sbcl-32-threaded/rebuild+))))
